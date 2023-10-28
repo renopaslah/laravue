133da9eb2ef11ebea143408305e51899ce0177f8
Reno Paslah <reno.nilam@gmail.com>
2023-10-28 14:36:22 +0700

    fixing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -69,9 +69,9 @@
                             <span class="avatar avatar-sm"
                                 style="background-image: url({{ asset('dist/static/avatars/000m.jpg') }})"></span>
                             <div class="d-none d-xl-block ps-2">
-                                <div>???</div>
+                                <div>Tony Stark</div>
                                 <div class="mt-1 small text-muted">
-                                    ???
+                                    Administrator
                                 </div>
                             </div>
                         </a>
@@ -106,6 +106,24 @@
                                     </span>
                                     <span class="nav-link-title">
                                         Beranda
+                                    </span>
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('posts.index') }}">
+                                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                        <!-- Download SVG icon from http://tabler-icons.io/i/home -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-news" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <path d="M16 6h3a1 1 0 0 1 1 1v11a2 2 0 0 1 -4 0v-13a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1v12a3 3 0 0 0 3 3h11"></path>
+                                            <path d="M8 8l4 0"></path>
+                                            <path d="M8 12l4 0"></path>
+                                            <path d="M8 16l4 0"></path>
+                                         </svg>
+                                    </span>
+                                    <span class="nav-link-title">
+                                        Post
                                     </span>
                                 </a>
                             </li>
